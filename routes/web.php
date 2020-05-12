ebb0ae711e2a9806fa90390d51dda615d5d597f8
Redi Linxa <redi.linxa@edcoms.co.uk>
2020-05-12 18:37:12 +0200

    feature - added user registration and login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function (){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/home', 'HomeController@index')->name('home');
 });
+
+
